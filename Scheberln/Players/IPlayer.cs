16e515bc2969314da493e8f72fb3b8a3b7dcb62a
Ganko Pi <ganko-pi@outlook.de>
2024-05-09 16:28:34 +0200

    style: remove public keywords from interface functions

--- Scheberln/Players/IPlayer.cs
+++ Scheberln/Players/IPlayer.cs
@@ -13,12 +13,12 @@ public interface IPlayer
     /// <summary>
     /// The cards the player currently holds.
     /// </summary>
-    public List<Card> Cards { get; set; }
+    List<Card> Cards { get; set; }
 
     /// <summary>
     /// Returns a card the player wants to play based on the current state of the game.
     /// </summary>
     /// <param name="gameState">The current state of the game such as all played cards and the dealer.</param>
     /// <returns>A card the player wants to play.</returns>
-    public Card Play(GameState gameState); 
+    Card Play(GameState gameState);
 }
