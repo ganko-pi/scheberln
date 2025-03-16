7629a564ad8073833ba1a6412d6c40bf0ab1977a
Ganko Pi <ganko-pi@outlook.de>
2025-03-16 10:50:29 +0100

    feat: add OrConstraint

---
+++ Scheberln/PlayValidators/Constraints/OrConstraint.cs
@@ -0,0 +1,30 @@
+using System.Linq;
+
+using Scheberln.Cards;
+using Scheberln.Game;
+using Scheberln.Players;
+
+namespace Scheberln.PlayValidators.Constraints;
+
+/// <summary>
+/// Implementation of <see cref="IConstraint"/> to combine multiple other <see cref="IConstraint"/>s.
+/// This constraint is fulfilled when at least one of the passed <see cref="IConstraint"/>s is fulfilled.
+/// </summary>
+public class OrConstraint : IConstraint
+{
+    private readonly IConstraint[] _constraints;
+
+    /// <summary>
+    /// Creates a new <see cref="OrConstraint"/> combining the passed <paramref name="constraints"/>.
+    /// </summary>
+    public OrConstraint(params IConstraint[] constraints)
+    {
+        _constraints = constraints;
+    }
+
+    /// <inheritdoc/>
+    public bool IsFullfilled(GameState gameState, IPlayer currentPlayer, Card? cardThePlayerWantsToPlay)
+    {
+        return _constraints.Any(constraint => constraint.IsFullfilled(gameState, currentPlayer, cardThePlayerWantsToPlay));
+    }
+}
